ca0b62c35d60ff037f070efc6678a52cf8075554
jonghun <jonghuncu@gmail.com>
2023-08-07 17:33:57 +0900

    티켓판매 어플리케이션 구현

---
+++ src/main/java/org/design/Invitation.java
@@ -0,0 +1,7 @@
+package org.design;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+    private LocalDateTime when;
+}
---
+++ src/main/java/org/design/Ticket.java
@@ -0,0 +1,9 @@
+package org.design;
+
+public class Ticket {
+    private Long fee;
+
+    public Long getFee() {
+        return fee;
+    }
+}
